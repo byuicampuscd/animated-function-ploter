6ac86dc5baa87e3a98fc4d71488caf5b0522bd81
Joshua McKinney <mckinneyj@byui.edu>
2016-07-07 20:55:00 -0600

    about to start making it work with Andrew's code

---
+++ addPoint.js
@@ -0,0 +1,61 @@
+/*jslint plusplus: true, browser: true, devel: true, evil:true*/
+/*global d3, functionPlot*/
+var addPoint = (function () {
+   "use strict";
+
+   return function (xIn, funPlot) {
+
+      function getY(x) {
+         return functionPlot.eval.builtIn(funPlot.options.data[0], 'fn', {
+            x: x
+         });
+      }
+
+      function updateTextX() {
+         var zero = (0).toFixed(2);
+
+         return function (t) {
+            var location = xIn * t;
+            this.textContent = "( " + location.toFixed(2) + ", " + zero + ")";
+         };
+      }
+
+      function updateTextY() {
+         var xRounded = xIn.toFixed(2),
+            yVal = getY(xIn);
+
+         return function (t) {
+            this.textContent = "( " + xRounded + ", " + (yVal * t).toFixed(2) + ")";
+         };
+      }
+
+      var svgSelector = funPlot.options.target + ' .content',
+         xScale = funPlot.meta.xScale,
+         yScale = funPlot.meta.yScale,
+         group = d3.select(svgSelector).append('g'),
+         cir = group.append('circle'),
+         label = group.append('text').text(""),
+         transition;
+
+      //center group
+      group.attr('transform', "translate(" + xScale(0) + " " + yScale(0) + ")");
+
+      //move label
+      label.attr('x', 5)
+         .attr('y', 15);
+      //add raduis
+      cir.attr('r', 4)
+         .attr('class', 'point');
+
+      //First transition - move the group in the X
+      transition = group.transition().duration(2000).ease('cubic-out').attr('transform', 'translate(' + xScale(xIn) + ' ' + yScale(0) + ')');
+      //sub transition - update the label
+      transition.select('text').tween('text', updateTextX);
+
+      //Second transition - move the group in the Y
+      //sub transition - update the label
+      transition.transition().attr('transform', 'translate(' + xScale(xIn) + ' ' + yScale(getY(xIn)) + ')')
+         .select('text').tween('text', updateTextY);
+
+   };
+}());
---
+++ aniOptions.js
@@ -0,0 +1,44 @@
+var aniOptions = {
+   "datapoints": [
+      {
+         "x": 1,
+         "y": 5,
+         "id": 0,
+         "changeEqu": "3(1)+2",
+         "updatePoint": false,
+         "element": {}
+      },
+      {
+         "x": 3,
+         "y": 11,
+         "id": 1,
+         "changeEqu": "3(3)+2",
+         "updatePoint": true,
+         "element": {}
+      },
+      {
+         "x": 3,
+         "y": 11,
+         "id": 2,
+         "changeEqu": "3(3)+2",
+         "updatePoint": false,
+         "element": {}
+      }
+   ],
+   "currentRound": 1,
+   "graphOpt": {
+      "animateHide": false,
+      "graphHide": false,
+      "equation": "3x+2",
+      "view": {
+         "x": {
+            "min": -10,
+            "max": 10
+         },
+         "y": {
+            "min": -10,
+            "max": 10
+         }
+      }
+   }
+};
