ed7c7fff72b642da63d96af4be0019861358ba7f
Joshua McKinney <mckinneyj@byui.edu>
2016-09-30 09:30:21 -0600

    i think the only stuff that changed was spacing

--- aniOptions.js
+++ aniOptions.js
@@ -1,97 +1,97 @@
 var aniOptions = {
-   "datapoints": [
-      {
-         "x": 1,
-         "y": 5,
-         "id": 0,
-         "changeEqu": "3(1)+2",
-         "updatePoint": true,
-         "element": {}
-      },
-      {
-         "x": 2,
-         "y": 8,
-         "id": 1,
-         "changeEqu": "3(2)+2",
-         "updatePoint": true,
-         "element": {}
-      },
-      {
-         "x": 3,
-         "y": 11,
-         "id": 2,
-         "changeEqu": "3(3)+2",
-         "updatePoint": true,
-         "element": {}
-      }
-   ],
-   "currentRound": 0,
-   "graphOpt": {
-      "animateHide": false,
-      "graphHide": false,
-      "equation": "3x+2",
-      "view": {
-         "x": {
-            "min": -15,
-            "max": 15
-         },
-         "y": {
-            "min": -15,
-            "max": 15
-         }
-      }
-   }
+    "datapoints": [
+        {
+            "x": 1,
+            "y": 5,
+            "id": 0,
+            "changeEqu": "3(1)+2",
+            "updatePoint": true,
+            "element": {}
+        },
+        {
+            "x": 2,
+            "y": 8,
+            "id": 1,
+            "changeEqu": "3(2)+2",
+            "updatePoint": true,
+            "element": {}
+        },
+        {
+            "x": 3,
+            "y": 11,
+            "id": 2,
+            "changeEqu": "3(3)+2",
+            "updatePoint": true,
+            "element": {}
+        }
+    ],
+    "currentRound": 0,
+    "graphOpt": {
+        "animateHide": false,
+        "graphHide": false,
+        "equation": "3x+2",
+        "view": {
+            "x": {
+                "min": -15,
+                "max": 15
+            },
+            "y": {
+                "min": -15,
+                "max": 15
+            }
+        }
+    }
 };
 
 var aniOptions2 = {
-   "datapoints": [
-      {
-         "x": 1,
-         "y": 5,
-         "id": 0,
-         "changeEqu": "3(1)+2",
-         "updatePoint": true,
-         "element": {}
-      },
-      {
-         "x": 2,
-         "y": 8,
-         "id": 1,
-         "changeEqu": "3(2)+2",
-         "updatePoint": true,
-         "element": {}
-      },
-      {
-         "x": 3,
-         "y": 11,
-         "id": 2,
-         "changeEqu": "3(3)+2",
-         "updatePoint": true,
-         "element": {}
-      },
-      {
-         "x": -1,
-         "y": -1,
-         "id": 2,
-         "changeEqu": "3(-1)+2",
-         "updatePoint": true,
-         "element": {}
-      }
-   ],
-   "currentRound": 0,
-   "graphOpt": {
-      "animateHide": false,
-      "graphHide": false,
-      "equation": "3x+2",
-      "view": {
-         "x": {
-            "min": -5,
-            "max": 5
-         },
-         "y": {
-            "min": -5,
-            "max": 5
-         }
-      }
-   }
+    "datapoints": [
+        {
+            "x": 1,
+            "y": 5,
+            "id": 0,
+            "changeEqu": "3(1)+2",
+            "updatePoint": true,
+            "element": {}
+        },
+        {
+            "x": 2,
+            "y": 8,
+            "id": 1,
+            "changeEqu": "3(2)+2",
+            "updatePoint": true,
+            "element": {}
+        },
+        {
+            "x": 3,
+            "y": 11,
+            "id": 2,
+            "changeEqu": "3(3)+2",
+            "updatePoint": true,
+            "element": {}
+        },
+        {
+            "x": -1,
+            "y": -1,
+            "id": 2,
+            "changeEqu": "3(-1)+2",
+            "updatePoint": true,
+            "element": {}
+        }
+    ],
+    "currentRound": 0,
+    "graphOpt": {
+        "animateHide": false,
+        "graphHide": false,
+        "equation": "3x+2",
+        "view": {
+            "x": {
+                "min": -5,
+                "max": 5
+            },
+            "y": {
+                "min": -5,
+                "max": 5
+            }
+        }
+    }
 };
